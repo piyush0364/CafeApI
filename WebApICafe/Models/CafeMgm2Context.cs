601f818560a06c32efe3f757859b4bb04a89ef0f
Piyush Tripathi <tripathipiyush456@gmail.com>
2024-11-15 00:14:09 +0530

    Update CafeMgm2Context.cs

--- WebApICafe/Models/CafeMgm2Context.cs
+++ WebApICafe/Models/CafeMgm2Context.cs
@@ -33,7 +33,7 @@ public partial class CafeMgm2Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:cafeapp.database.windows.net,1433;Initial Catalog=CafeMgm2;Persist Security Info=False;User ID=piyush;Password=google@456;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer("Server=tcp:cafe2.database.windows.net,1433;Initial Catalog=CafeMgm2;Persist Security Info=False;User ID=piyush;Password=google@456;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
